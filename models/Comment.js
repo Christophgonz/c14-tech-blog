e561ec552f992866757dcb8b0fb944cae71a30db
cgonz <christoph.gonz@gmail.com>
2023-05-30 01:36:59 -0700

    setup model relationships

--- models/Comment.js
+++ models/Comment.js
@@ -19,6 +19,20 @@ Post.init({
     type: DataTypes.STRING,
     allowNull: false,
   },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "user",
+      key: "id",
+    },
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "post",
+      key: "id",
+    },
+  },
   sequelize,
   timestamps: false,
   freezeTableName: true,
