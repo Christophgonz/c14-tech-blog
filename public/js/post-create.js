a9a8d22c6fed72958df562a14456d13bbf165f6b
cgonz <christoph.gonz@gmail.com>
2023-05-30 22:55:34 -0700

    fixed post create

--- public/js/post-create.js
+++ public/js/post-create.js
@@ -18,22 +18,6 @@ const createPostHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to delete project");
-    }
-  }
-};
-
 document
   .querySelector(".create-post")
   .addEventListener("submit", createPostHandler);
